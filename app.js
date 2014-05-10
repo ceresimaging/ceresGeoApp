12aeda4667921d493c88f47f0e93759fcabb0e01
huaruiwu <huarui.wu@gmail.com>
2014-05-09 23:38:41 -0700

    add some functions to try

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 $(function(){
 
   var $position = $('#position');
+  var $pntA = $('#pnt-A');
+  var $pntB = $('#pnt-B');
+  var $btnA = $('#btn-A');
+  var $btnB = $('#btn-B');
   var wpid = null;
 
   var geo_options = {
     enableHighAccuracy: true,
   };
-
 
   function geo_success( position ) {
     $position.html( 'lat: '+position.coords.latitude +
@@ -19,6 +22,23 @@ $(function(){
     $position.html('error');
   }
 
+  $btnA.click(function(e) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $pntA.html( 'lat: '+position.coords.latitude +
+                    ' long: '+position.coords.longitude +
+                    ' accuracy: '+position.coords.accuracy);
+    });
+  });
+
+  $btnB.click(function(e) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $pntB.html( 'lat: '+position.coords.latitude +
+                    ' long: '+position.coords.longitude +
+                    ' accuracy: '+position.coords.accuracy);
+    });
+  });
 
   navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
