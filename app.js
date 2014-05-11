322ab137166db72bb471a692668d39db56ef27e9
huaruiwu <huarui.wu@gmail.com>
2014-05-10 23:43:45 -0700

    add organization

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+function App(){
+
+  var app = this;
+
+  function showLocation(el) {
+    return function(position) {
+      el.html( 'lat: '+position.coords.latitude +
+                    ' long: '+position.coords.longitude +
+                    ' accuracy: '+position.coords.accuracy);
+    }
+  }
+  function errorCallback(){
+  }
+
+  this.watchID = null;
+
+  this.getLocation = function(el) {
+    navigator.geolocation.getCurrentPosition(showLocation(el));
+  };
+
+  this.watchLocation = function(el) {
+    app.watchID = navigator.geolocation.watchPosition(showLocation(el),
+        errorCallback, { enableHighAccuracy: true });
+  };
+
+}
+
 $(function(){
 
   var $position = $('#position');
@@ -5,41 +32,16 @@ $(function(){
   var $pntB = $('#pnt-B');
   var $btnA = $('#btn-A');
   var $btnB = $('#btn-B');
-  var wpid = null;
 
-  var geo_options = {
-    enableHighAccuracy: true,
-  };
-
-  function geo_success( position ) {
-    $position.html( 'lat: '+position.coords.latitude +
-                    ' long: '+position.coords.longitude +
-                    ' accuracy: '+position.coords.accuracy);
-
-  }
-
-  function geo_error() {
-    $position.html('error');
-  }
-
-  $btnA.click(function(e) {
+  var app = new App();
+  app.watchLocation($position);
+  $btnA.click(function(e){
     e.preventDefault();
-    navigator.geolocation.getCurrentPosition(function(position) {
-      $pntA.html( 'lat: '+position.coords.latitude +
-                    ' long: '+position.coords.longitude +
-                    ' accuracy: '+position.coords.accuracy);
-    });
+    app.getLocation($pntA);
   });
-
-  $btnB.click(function(e) {
+  $btnB.click(function(e){
     e.preventDefault();
-    navigator.geolocation.getCurrentPosition(function(position) {
-      $pntB.html( 'lat: '+position.coords.latitude +
-                    ' long: '+position.coords.longitude +
-                    ' accuracy: '+position.coords.accuracy);
-    });
+    app.getLocation($pntB);
   });
-
-  navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
 })
