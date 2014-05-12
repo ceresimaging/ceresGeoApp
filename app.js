095112095fa6cada603cd7d9b3ae3e360c530e34
huaruiwu <huarui.wu@gmail.com>
2014-05-12 13:35:31 -0700

    add get distance between two points

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+Number.prototype.toRad = function() {
+  return this * Math.PI / 180;
+}
+
 function App(){
 
   var app = this;
@@ -9,11 +13,30 @@ function App(){
   }
   function errorCallback(){
   }
+  // adapted from Moveable Type
+  // www.movable-type.co.uk/scripts/latlong.html
+  function getPntDist(a, b){
+    var R = 6371;
+    var lat1 = a.coords.latitude;
+    var lat2 = b.coords.latitude;
+    var lng1 = a.coords.longitude;
+    var lng2 = b.coords.longitude;
+
+    var dLat = (lat2 - lat1).toRad();
+    var dLng = (lng2 - lng1).toRad();
+    var a = Math.sin(dLat / 2) * Math.sin(dLat /2) +
+            Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+            Math.sin(dLng / 2) * Math.sin(dLng / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d;
+  }
 
   this.watchID = null;
   this.posCurrent = null;
   this.posA = null;
   this.posB = null;
+  this.distance = null;
 
   this.getLocationA = function(el) {
     navigator.geolocation.getCurrentPosition(function(position){
@@ -33,6 +56,7 @@ function App(){
     app.watchID = navigator.geolocation.watchPosition(function(position){
       showLocation(el, position);
       app.posCurrent = position;
+      console.log(getPntDist(app.posA, app.posB));
     }, errorCallback, { enableHighAccuracy: true });
   };
 
